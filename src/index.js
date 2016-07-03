6a0c0a6eacc0cc598ed7793aaaad3243d6d38cca
welefen <welefen@gmail.com>
2016-07-03 19:36:44 +0800

    Release 1.0.0

---
+++ src/index.js
@@ -0,0 +1,54 @@
+import Plugin from 'stc-plugin';
+
+/**
+ * default css compress options
+ */
+const compressOpts = {
+  removeComment: true,
+  removeLastSemicolon: true, 
+  removeEmptySelector: true, 
+  overrideSameProperty: true, 
+  shortValue: true, 
+  mergeProperty: true, 
+  sortProperty: true, 
+  sortSelector: true, 
+  mergeSelector: true, 
+  propertyToLower: true 
+};
+
+let options = null;
+
+/**
+ * compress css class
+ */
+export default class CssCompressPlugin extends Plugin {
+  /**
+   * run
+   */
+  async run(){
+    if(!options){
+      options = {...compressOpts, ...this.config.tpl, ...this.options};
+    }
+    let ast = await this.getAst();
+    let instance = this.stc.flkit.getFlkitInstance('CssCompress', ast, options);
+    return instance.run(true);
+  }
+  /**
+   * update
+   */
+  update(ast){
+    this.setAst(ast);
+  }
+  /**
+   * use cluster
+   */
+  static cluster(){
+    return true;
+  }
+  /**
+   * use cache
+   */
+  static cache(){
+    return true;
+  }
+}
